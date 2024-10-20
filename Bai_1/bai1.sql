fdeb3645af64447e73802991433b809c815d78f7
loc813 <111342133+loc813@users.noreply.github.com>
2024-10-20 16:52:00 +0700

    add bai tap

---
+++ Bai_1/bai1.sql
@@ -0,0 +1,6 @@
+-- chon tat ca cac cot cua bang BOOKs --
+select * from Books 
+
+-- chon ten sach va tac gia cua nhung cuon sach co gia > 50 -- 
+select Title , Author from Books 
+where Price > 50 ; 
